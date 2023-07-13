9e3ebd672dc9624cf07a517868fd67fe173cabd1
Eduardo <eduardowz.prog@gmail.com>
2023-07-13 09:12:26 -0300

    chore: rubocop offense adjust

--- config/environment.rb
+++ config/environment.rb
@@ -2,8 +2,8 @@
 
 require 'rubygems'
 require 'bundler'
-Bundler.require(:default)
-Bundler.require(Sinatra::Base.environment)
+Bundler.require(:default)                   # load all the default gems
+Bundler.require(Sinatra::Base.environment)  # load all the environment specific gems
 require 'active_support/deprecation'
 require 'active_support/all'
 Dotenv.load
